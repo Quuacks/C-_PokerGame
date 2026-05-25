49c336178b3bd63b6e587c5e9de2adcf431cd4a1
Quuacks <staneviciusarmantas@gmail.com>
2026-05-25 09:50:51 +0300

    Kolkas kad butu lengviau UI daryt uzkomentavau prisijungima prie servo, kad UI pasileistu

--- Server/src/messageHandlers/requestHandler.h
+++ Server/src/messageHandlers/requestHandler.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <string>
+#include <nlohmann/json.hpp>
+#include <winsock2.h>
+
+class Player;
+
 class RequestHandler
 { 
-public:
-    virtual void Execute(std::string message) = 0;
+    public:
+        virtual ~RequestHandler() = default;
 
+        virtual void ExecuteRaw(SOCKET rawSocket, const json& data){}
 
+        virtual void ExecutePlayer(Player& player, const json& data){}
 };
 
