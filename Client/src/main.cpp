7b398f40b899fe372ee34f1f2145ea5e36d90585
Mykolas Liandzbergas <mykolas.liandzbergas@mif.stud.vu.lt>
2026-05-26 00:30:28 +0300

    Padarytas login page

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -19,6 +19,23 @@
 
 using json = nlohmann::json;
 
+#define ID_LOGIN_EDIT      6001
+#define ID_LOGIN_BUTTON    6002
+#define ID_CANCEL_BUTTON   6003
+
+struct LoginPromptData
+{
+    std::wstring username;
+    bool done = false;
+    bool cancelled = false;
+    HWND editBox = nullptr;
+};
+
+std::string WideToUtf8(const std::wstring& text);
+std::wstring TrimUsername(const std::wstring& text);
+LRESULT CALLBACK LoginWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+std::string ShowLoginPage(HINSTANCE hInstance);
+
 bool TryReadBoolTurnField(const json& data, const char* key, bool& result)
 {
     if (data.contains(key) && data[key].is_boolean())
@@ -103,6 +120,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     InitCommonControlsEx(&icex);
 
     // 4. Initialize Network Client instance
+    std::string localUsername = ShowLoginPage(hInstance);
+
+    if (localUsername.empty())
+    {
+        std::cout << "[CLIENT] Login cancelled.\n";
+
+        if (fp)
+            fclose(fp);
+
+        FreeConsole();
+        WSACleanup();
+        return 0;
+    }
+
     NetworkClient client;
     SetNetworkClientForUI(&client); // Pass the client pointer to UI backend!
 
@@ -113,8 +144,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     // 5. Send initial JSON login request packet
-    const std::string localUsername = "Quuacks";
-
     json loginPacket = {
         {"type", "LOGIN"},
         {"data", {{"username", localUsername}}}
@@ -134,6 +163,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     HWND mainHWnd = FindWindowW(L"PokerUI", L"Poker Game");
 
     AddStatusMessage(L"Connected to server.");
+    AddStatusMessageFromUtf8("Logged in as: " + localUsername);
     AddStatusMessage(L"Login request sent.");
     AddStatusMessage(L"Client is ready.");
 
@@ -181,4 +211,234 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     WSACleanup();
 
     return (int)msg.wParam;
+}
+
+std::string WideToUtf8(const std::wstring& text)
+{
+    if (text.empty())
+        return "";
+
+    int sizeNeeded = WideCharToMultiByte(
+        CP_UTF8,
+        0,
+        text.c_str(),
+        (int)text.size(),
+        nullptr,
+        0,
+        nullptr,
+        nullptr
+    );
+
+    if (sizeNeeded <= 0)
+        return "";
+
+    std::string result(sizeNeeded, '\0');
+
+    WideCharToMultiByte(
+        CP_UTF8,
+        0,
+        text.c_str(),
+        (int)text.size(),
+        &result[0],
+        sizeNeeded,
+        nullptr,
+        nullptr
+    );
+
+    return result;
+}
+
+std::wstring TrimUsername(const std::wstring& text)
+{
+    size_t start = text.find_first_not_of(L" \t\r\n");
+    size_t end = text.find_last_not_of(L" \t\r\n");
+
+    if (start == std::wstring::npos)
+        return L"";
+
+    return text.substr(start, end - start + 1);
+}
+
+LRESULT CALLBACK LoginWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    LoginPromptData* data = (LoginPromptData*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+
+    switch (msg)
+    {
+    case WM_NCCREATE:
+    {
+        CREATESTRUCT* createStruct = (CREATESTRUCT*)lParam;
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)createStruct->lpCreateParams);
+        return TRUE;
+    }
+
+    case WM_CREATE:
+    {
+        data = (LoginPromptData*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+
+        CreateWindowW(
+            L"STATIC",
+            L"Enter your username:",
+            WS_CHILD | WS_VISIBLE,
+            25,
+            25,
+            300,
+            25,
+            hWnd,
+            nullptr,
+            nullptr,
+            nullptr
+        );
+
+        data->editBox = CreateWindowExW(
+            WS_EX_CLIENTEDGE,
+            L"EDIT",
+            L"",
+            WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
+            25,
+            55,
+            300,
+            26,
+            hWnd,
+            (HMENU)ID_LOGIN_EDIT,
+            nullptr,
+            nullptr
+        );
+
+        SendMessage(data->editBox, EM_SETLIMITTEXT, 24, 0);
+
+        CreateWindowW(
+            L"BUTTON",
+            L"Login",
+            WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+            95,
+            100,
+            90,
+            32,
+            hWnd,
+            (HMENU)ID_LOGIN_BUTTON,
+            nullptr,
+            nullptr
+        );
+
+        CreateWindowW(
+            L"BUTTON",
+            L"Cancel",
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            195,
+            100,
+            90,
+            32,
+            hWnd,
+            (HMENU)ID_CANCEL_BUTTON,
+            nullptr,
+            nullptr
+        );
+
+        SetFocus(data->editBox);
+        return 0;
+    }
+
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wParam))
+        {
+        case ID_LOGIN_BUTTON:
+        {
+            wchar_t buffer[128];
+            GetWindowTextW(data->editBox, buffer, 128);
+
+            std::wstring username = TrimUsername(buffer);
+
+            if (username.empty())
+            {
+                MessageBoxW(
+                    hWnd,
+                    L"Please enter a username.",
+                    L"Username required",
+                    MB_OK | MB_ICONWARNING
+                );
+
+                SetFocus(data->editBox);
+                return 0;
+            }
+
+            data->username = username;
+            data->done = true;
+            DestroyWindow(hWnd);
+            return 0;
+        }
+
+        case ID_CANCEL_BUTTON:
+            data->cancelled = true;
+            data->done = true;
+            DestroyWindow(hWnd);
+            return 0;
+        }
+
+        break;
+    }
+
+    case WM_CLOSE:
+        data->cancelled = true;
+        data->done = true;
+        DestroyWindow(hWnd);
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+std::string ShowLoginPage(HINSTANCE hInstance)
+{
+    const wchar_t* loginClassName = L"PokerLoginWindow";
+
+    WNDCLASSEXW wc{};
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.lpfnWndProc = LoginWndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = loginClassName;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    RegisterClassExW(&wc);
+
+    LoginPromptData data;
+
+    HWND loginWindow = CreateWindowExW(
+        WS_EX_DLGMODALFRAME,
+        loginClassName,
+        L"Poker Login",
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        370,
+        180,
+        nullptr,
+        nullptr,
+        hInstance,
+        &data
+    );
+
+    if (!loginWindow)
+        return "";
+
+    ShowWindow(loginWindow, SW_SHOW);
+    UpdateWindow(loginWindow);
+
+    MSG msg{};
+
+    while (!data.done && GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!IsDialogMessage(loginWindow, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    if (data.cancelled)
+        return "";
+
+    return WideToUtf8(data.username);
 }
